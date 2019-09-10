afdce633ece0df99422a436772fe127dd62706a2
Niracler <1026037967@qq.com>
2019-09-10 11:23:09 +0800

    net_log

---
+++ crawler/spiders/net_log_redis.py
@@ -0,0 +1,19 @@
+from crawler.items import NetLogItem
+from scrapy_redis.spiders import RedisCrawlSpider
+
+
+class NetLogSpider(RedisCrawlSpider):
+    name = 'net_log'
+    redis_key = 'net_log:start_urls'
+    item_index = 'url'
+    custom_settings = {
+        'MONGODB_COLLECTION': 'net_log',
+        'DOWNLOAD_DELAY': 0,
+        'DOWNLOAD_TIMEOUT': 5,
+    }
+
+    def parse(self, response):
+        net_log = NetLogItem()
+        net_log['url'] = response.url
+        net_log['urldata'] = response.body
+        yield net_log
